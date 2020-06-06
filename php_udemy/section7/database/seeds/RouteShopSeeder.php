20013e0fc82b5c0384075b643cbe0180ee0b50d7
SomaSekimoto <0421.soma@gmail.com>
2020-06-06 16:52:58 +0900

    cmd: php artisan make:seed RouteShopSeeder

---
+++ php_udemy/section7/database/seeds/RouteShopSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RouteShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
