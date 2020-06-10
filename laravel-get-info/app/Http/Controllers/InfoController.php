92de86102b7010e70f99271a9728a12a47733d1f
SomaSekimoto <0421.soma@gmail.com>
2020-06-10 16:06:10 +0900

    set: ip function

--- laravel-get-info/app/Http/Controllers/InfoController.php
+++ laravel-get-info/app/Http/Controllers/InfoController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Info;
 
 class InfoController extends Controller
 {
     //
+    public function ip(){
+        // $ips = []
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip); // just to be safe
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return view('ip', compact('ip'));
+                    }
+                    return view('ip', compact('ip'));
+                    // $ips[$key] = $ip 
+                }
+            }
+        }
+        // return view('ip', compact('ips'));
+    }
+
+    public function anything(){
+        return view('anything');
+    }
 }
