3215b33468943df1611e310736c26322f32e6a79
SomaSekimoto <0421.soma@gmail.com>
2020-06-11 16:17:32 +0900

    change: ip & anything functions

--- laravel-get-info/app/Http/Controllers/InfoController.php
+++ laravel-get-info/app/Http/Controllers/InfoController.php
@@ -8,33 +8,20 @@ use App\Models\Info;
 class InfoController extends Controller
 {
     //
-    public function ip(){
-        // $ips = []
-        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-            if (array_key_exists($key, $_SERVER) === true){
-                foreach (explode(',', $_SERVER[$key]) as $ip){
-                    $ip = trim($ip); // just to be safe
-                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-                        return view('ip', compact('ip'));
-                    }
-                    return view('ip', compact('ip'));
-                    // $ips[$key] = $ip 
-                }
-            }
-        }
-        // return view('ip', compact('ips'));
+    public function ip(Request $request){
+        
+        return response()->json(["origin" => $request->ip()]);
     }
 
     public function anything(Request $request){
 
-        $headers = $request;
-        $url = $request->url();
+        $host = $request->header('host');
+        $url = $request->fullUrl();
         $method = $request->method();
         $userAgent = $request->userAgent();
         $origin = $request->ip();
+        $args = $request->query();
 
-        return view('anything', compact('headers', 'url', 'method', 'userAgent', 'origin'));
-        // return view('anything', compact('request'));
-        // dd($request);
+        return response()->json(["args" => $args, "headers" => ["Host" => $host, "User-Agent" => $userAgent ], "method" => $method, "origin" => $origin, "url" => $url ]);
     }
 }
