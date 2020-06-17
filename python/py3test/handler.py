0540eec5b58ec127936bb77868aa4c2c04f694c6
SomaSekimoto <0421.soma@gmail.com>
2020-06-17 20:26:12 +0900

    add: urlib & create function comparing both responses

--- python/py3test/handler.py
+++ python/py3test/handler.py
@@ -1,24 +1,26 @@
 import json
+import urllib.request
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    bin_ip = "https://httpbin.org/ip"
+    r = urllib.request.Request(bin_ip)
+
+    myapp_ip = "http://13.114.122.230/ip"
+    s = urllib.request.Request(myapp_ip)
+
+    with urllib.request.urlopen(r) as res_r:
+        r_result = json.loads(res_r.read())
+
+    with urllib.request.urlopen(s) as res_s:
+        s_result = json.loads(res_s.read())
+
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "r": r_result, 
+        "s": s_result,
+        "result": r_result == s_result
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
