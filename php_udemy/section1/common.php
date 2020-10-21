7cf15d10557c1b3ed3ea2a64b6b4f3b14465f523
SomaSekimoto <0421.soma@gmail.com>
2020-10-21 22:47:28 +0900

    remove: folders

--- php_udemy/section1/common.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-
-<head></head>
-<body>
-<?php
-  $commonVariable = '共通の変数';
-?>
-</body>
-</html>
--- php_udemy/section6/resources/views/tests/test.blade.php
+++
@@ -1,8 +0,0 @@
-test success<br>
-
-<!-- '{}'を二重にすることで、XSS攻撃を防ぐため、自動でphpのhtmlspecialchars関数を通される -->
-@foreach($values as $value)
-  
-  {{$value->id}}<br>
-  {{$value->text}}<br>
-@endforeach
