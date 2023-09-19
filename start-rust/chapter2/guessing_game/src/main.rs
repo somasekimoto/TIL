8b5d11c75942e4c28fce9750d8913ede38556cb1
somasekimoto <0421.soma@gmail.com>
2023-09-20 00:13:07 +0900

    done: chapter2 in start-rust

--- start-rust/chapter2/guessing_game/src/main.rs
+++ start-rust/chapter2/guessing_game/src/main.rs
@@ -1,15 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");          // 数を当ててごらん
+    println!("Guess the number!");
 
-    println!("Please input your guess.");   // ほら、予想を入力してね
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");     // 行の読み込みに失敗しました
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);     // 次のように予想しました: {}
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
